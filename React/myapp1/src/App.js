f68011a7e80dbcf6194976cd5fb1264fd5e6d182
PoFoq <pofoq@mail.ru>
2022-08-24 17:58:09 +0300

    lesson02

--- React/myapp1/src/App.js
+++ React/myapp1/src/App.js
@@ -1,8 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
 import Message from './Message';
+import { useState, useRef, useEffect } from 'react';
 
 function App() {
+  const [messageList, setMessageList] = useState([])
+  const ref = useRef(null)
+
+  const sendMessageHandle = (event) => {
+    event.preventDefault();
+    setMessageList(prev => [...prev, {
+      id: getLastId(prev) + 1,
+      author: 'user',
+      text: ref.current.value
+    }]);
+  }
+
+  function getLastId(array) {
+    return array.length ? array[array.length - 1].id : 0;
+  }
+
+  useEffect(() => {
+    const id = getLastId(messageList);
+    if (id > 0 && messageList.find(el => el.id === id).author !== 'bot') {
+      setTimeout(() => {
+        setMessageList(prev => [...prev, {
+          id: getLastId(prev) + 1,
+          author: 'bot',
+          text: 'I am a Robot'
+        }]);
+      }, 1500);
+    }
+  }, [messageList]);
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -19,9 +50,25 @@ function App() {
         >
           Learn React
         </a>
+        <div>
+          <input ref={ref} />
+          <button onClick={sendMessageHandle}>Send</button>
+          {messageList.map((item) => {
+            return (
+              <div className='Message_list' key={item.id}>
+                <div className='Message_box Message_Author'>
+                  {item.author}:
+                </div>
+                <div className='Message_box Message_Text'>
+                  {item.text}
+                </div>
+              </div>
+            )
+          })}
+        </div>
       </header>
     </div>
-  );
+  )
 }
 
 export default App;
